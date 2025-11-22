27173338ebe455f957591b43bf940f08dca77c11
Manohar-mrtarns <https://github.com/Manohar-mrtarns/dem.git>
2025-11-21 18:00:54 -1200

    ss

--- .gitignore/1.py
+++ .gitignore/1.py
@@ -1,1 +1,28 @@
-print("by")
+# print("by")
+# hghghg
+# gh
+# globalg
+# ghg
+# hasattrhg
+# h
+# ghg
+# hg
+# globalg
+# hgh
+# globalhg
+# hg
+# g
+# hg
+# globalhg
+# hg
+# hg
+# globalg
+# globalg
+
+# hg
+# globalgg
+
+# gh
+# hasattrh
+
+# h
